c7bd6e1ce1d500956372187829a70cf82da2da1c
武扬 <956826374@qq.com>
2018-10-12 16:49:13 +0800

    增加底部菜单

--- app/Router.js
+++ app/Router.js
@@ -5,6 +5,7 @@
  */
 import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation'
+import Icons from 'react-native-vector-icons/Ionicons'
 
 import Favorite from './view/Favorite'
 import Popular from './view/Popular'
@@ -17,5 +18,27 @@ export default createBottomTabNavigator({
     Trending,
     My,
 },{
-
+    initialRouteName: 'Favorite',
+    navigationOptions: ({navigation}) => ({
+        tabBarIcon: ({focused, horizontal, tintColor}) => {
+            const {routeName} = navigation.state;
+            let iconName;
+            if (routeName === 'Favorite') {
+                iconName = 'ios-keypad'
+            } else if (routeName === 'Popular') {
+                iconName = 'ios-ribbon'
+            } else if (routeName === 'Trending') {
+                iconName = 'ios-heart'
+            } else if (routeName === 'My') {
+                iconName = 'ios-person'
+            }
+            return <Icons name={iconName} size={horizontal ? 20 : 25} color={tintColor}/>
+        },
+    }),
+    tabBarOptions:{
+        activeTintColor: '#e91e63',
+        labelStyle: {
+            fontSize: 12,
+        },
+    },
 })
