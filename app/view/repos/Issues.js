8de1e5c62dce115554a075b3321ad5d41681a77f
武扬 <956826374@qq.com>
2018-11-01 13:36:00 +0800

    增加issues接口请求

--- app/view/repos/Issues.js
+++ app/view/repos/Issues.js
@@ -5,9 +5,11 @@
  * @flow
  */
 import React, {Component} from 'react'
-import {Container, View, Text, Segment, Button} from 'native-base'
+import {Container, View, Text, Segment, Button, ListItem, Body, Right} from 'native-base'
+import {FlatList} from 'react-native'
 import {connect} from 'react-redux'
 import Header,{ LeftReturn } from '../../component/Header'
+import _ from 'lodash';
 
 import BaseComponent from '../../component/BaseComponent'
 import ThemeSegment from '../../component/ThemeSegment'
@@ -19,28 +21,48 @@ class ViewScreen extends BaseComponent {
         this.state = {
             tabData: [
                 {
-                    text: 'Open',
+                    text: 'open',
                     active: true,
                 },{
-                    text: 'Closed',
+                    text: 'closed',
                 }
-            ]
+            ],
+            issueeData: [{},{}],
         };
         const {navigation} = this.props;
         this.repoData = navigation.getParam('repoData');
-        console.log(this.repoData);
     }
 
     async componentDidMount(){
         await super.componentDidMount();
-        if(isLogin(this.props.userInfo)){
-
-        }
+        const issuesList = await this.fetchIssuesList({
+            state: 'open',
+            page:1,
+            per_page: 10,
+        });
+        console.log('------获取issues列表-----');
+        console.log(issuesList);
     }
     changeTab = text => {
         this.setState({
             tabData: this.state.tabData.map(tab => ({...tab, active: tab.text === text}))
         })
+    }
+    _renderItem = (data) => {
+        return (
+            <ListItem>
+                <Body>
+                    <Text>Kumar Pratik</Text>
+                    <Text note>Doing what you like will always keep you happy . .</Text>
+                </Body>
+                <Right>
+                    <Text note>3:43 pm</Text>
+                </Right>
+            </ListItem>
+        )
+    }
+    fetchIssuesList = (options) => {
+        return this.github.getIssues(this.repoData.owner.login, this.repoData.name).listIssues(options)
     }
     render() {
         return (
@@ -53,6 +75,11 @@ class ViewScreen extends BaseComponent {
                     buttons={this.state.tabData}
                     onChangeTab={tab => this.changeTab(tab)}
                 />
+                <FlatList
+                    renderItem={this._renderItem}
+                    data={this.state.issueeData}
+                    keyExtractor={data => data.toString()}
+                />
             </Container>
         )
     }
@@ -61,6 +88,7 @@ const mapStateToProps = state => {
     return {
         backgroundColor: state.theme.color,
         showItem: state.favorite.showItem,
+        userInfo: state.userInfo,
     }
 };
 export default connect(mapStateToProps)(ViewScreen)
